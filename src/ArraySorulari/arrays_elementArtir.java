30b0c91f27444be6acb7d1f697c892a569b11a73
Sumeyra Gunel <61153720+emrahgx@users.noreply.github.com>
2023-03-29 00:35:06 +0300

    29 mart

--- src/ArraySorulari/arrays_elementArtir.java
+++ src/ArraySorulari/arrays_elementArtir.java
@@ -2,7 +2,7 @@ package ArraySorulari;
 
 import java.util.Arrays;
 
-public class soruCoz {
+public class arrays_elementArtir {
     public static void main(String[] args) {
         //Verilen bir int array’in tum elemanlarini 2 artirip bize donduren bir method
         //olusturun. Eski array’i yeni haliyle kaydedin.
---
+++ src/emrah.java
@@ -0,0 +1,5 @@
+public class emrah {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
