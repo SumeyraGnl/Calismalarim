f24e801f1c152723e5ca4ea8dbcda9c338415d07
Sümeyra Günel <sumeyragunel@Sumeyra-MacBook-Pro.local>
2023-04-18 12:11:11 +0300

    18 Nisan

---
+++ src/YeniCalisma/Ders.java
@@ -0,0 +1,47 @@
+package YeniCalisma;
+
+public class Ders {
+
+    int sayi;
+    String isim="Gulsum";
+    int deger=30;
+
+
+    @Override
+    public String toString() {
+        return "Ders{" +
+                "sayi=" + sayi +
+                ", isim='" + isim + '\'' +
+                ", deger=" + deger +
+                '}';
+    }
+
+    public Ders(int syi, String ism, int degr){
+
+        sayi=10;
+        isim="Rana";
+        deger=12;
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Ders obj=new Ders(3,"Rana",10  );
+        System.out.println(obj);
+
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
