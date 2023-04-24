7cd8d7b40813f4adc6549014687dfc1a355f40ea
SumeyraGnl <h.berengunel@gmail.com>
2023-04-25 00:10:27 +0300

    24 Nisan

--- src/YeniCalisma/Ders.java
+++ src/YeniCalisma/Ders.java
@@ -32,6 +32,7 @@ public class Ders {
         Ders obj=new Ders(3,"Rana",10  );
         System.out.println(obj);
         System.out.println("deneme");
+        System.out.println("deneme2");
     }
 
     }
